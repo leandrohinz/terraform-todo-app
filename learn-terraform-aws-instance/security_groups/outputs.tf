d994e2ba1a37df70397f465f35cbcb5876bcde4a
leandrohinz <91966964+leandrohinz@users.noreply.github.com>
2023-11-20 11:47:14 -0500

    Delete learn-terraform-aws-instance directory

--- learn-terraform-aws-instance/security_groups/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "security_group_id" {
-  value = aws_security_group.server_sg.id
-}
--- learn-terraform-aws-instance/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-  type        = string
-  description = "Region donde se desplegaran los recursos"
-  default     = "us-west-1"
-}
-
-
-
